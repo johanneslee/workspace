2360b08ad6a966abcfe7a0aaef0e034158710f46
Johannes Lee <sj202117@gmail.com>
2017-10-24 11:22:48 +0000

    update all

--- server.js
+++ server.js
@@ -3,46 +3,29 @@
 */
 var express = require('express');
 var http = require('http');
-//body-parser extract post data
-//body-parser give body attribute to request instance
 var bodyParser = require('body-parser');
-//express-session module can access cookie directly
-//cookie-parser module doesn't need anymore
 var session = require('express-session');
-//fs module can access and open file
 var fs = require("fs");
 
 /*
     Create a server
 */
 var app = express();
-//bind port
-var server = http.createServer(app).listen(8080, function(req, res){
-    console.log("Express server has started on port 8080");
-});
-var io = require('socket.io')(server).listen(8081);
-
-/*
-    Set middleware
-*/
-//define path to access html document to make the server can read them
+app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').renderFile);
-//define path to access static files to make the server can read them
 app.use(express.static('public'));
-//bodyParser settings
-//router must place below these settings
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
+var http = http.createServer(app);
+
 /*
-app.use(session({
-    secret: '!@johannesTB@!',
-    resave: false,
-    saveUninitialized: true
-}));
+    About socket.io
 */
-//about socket.io
+var io = require('socket.io').listen(http);
+
 io.on('connection',function(socket){
     console.log('A user connected');
     socket.on('join', function(data) {
@@ -61,5 +44,15 @@ io.on('connection',function(socket){
         console.log('user disconnected');
     });
 });
-//get a router module to the app
+
+/*
+    get a router module to the app
+*/
 var router = require("./routes/index.js")(app, fs);
+
+/*
+    Listening
+*/
+http.listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
